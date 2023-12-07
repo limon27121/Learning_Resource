00f872050de8147a48ff524528df1f6872a2717e
Hunter <moontaser009@gmail.com>
2023-12-07 16:47:25 +0600

    added form for adding resource

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,17 +1,77 @@
 <template>
-<BaseCard>
- <div>
-  <h2>hello</h2>
- </div>
- </BaseCard>
+  <base-card>
+  <div class="card">
+    <form>
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="3"></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </div>
+  </base-card>
 </template>
 
-<script>
-export default {
+<style scoped>
+  form {
+    max-width: 400px;
+    margin: 0 auto;
+  }
 
-}
-</script>
+  .form-control {
+    margin-bottom: 20px;
+  }
 
-<style>
+  label {
+    display: block;
+    margin-bottom: 5px;
+  }
 
+  input[type="text"],
+  input[type="url"],
+  textarea {
+    width: 100%;
+    padding: 8px;
+    margin-top: 5px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+
+  textarea {
+    resize: vertical;
+  }
+
+  base-button {
+    background-color: #3498db;
+    color: #fff;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border: none;
+    border-radius: 4px;
+  }
+
+  base-button:hover {
+    background-color: #2980b9;
+  }
+  .card {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 14px 18px rgba(0, 0, 0, 0.1);
+    background-color: #fff;
+  }
 </style>
